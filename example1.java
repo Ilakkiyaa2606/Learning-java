3d4606b5009d99d6f40977953e7cf5e68910887b
Ilakkiyaa <ilakkiyaajaisankar@gmail.com>
2025-06-26 12:02:45 +0530

    Add example1.java

---
+++ example1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class example1{
+    public static void main(String args[])
+    {
+      Scanner scan= new Scanner(System.in);
+      String name= scan.nextLine();
+      int age =scan.nextInt();
+      System.out.print("My name is Ilakkiyaa");
+      System.out.print("My age is 19");  
+    }
+}
--- input.java
+++ input.java
@@ -1,1 +1,12 @@
-class hello
+import java.lang.System;
+import java.util.Scanner;
+
+class hello{
+    public static void main(String args[])
+    {
+        Scanner var= new Scanner(System.in);
+        int a=var.nextInt();
+        int b=var.nextInt();
+        System.out.print(a+b);
+    }
+}
