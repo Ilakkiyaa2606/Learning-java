22fa48961780ccbafab46908fc416a6931cbd147
Ilakkiyaa <ilakkiyaajaisankar@gmail.com>
2025-06-26 13:13:45 +0530

    Add ex2.java

--- ex2.java
+++ ex2.java
@@ -6,11 +6,12 @@ class ex2 {
         Scanner scan = new Scanner(System.in);
         String name= scan.nextLine();
         int age= scan.nextInt();
+        scan.nextLine();
         String address= scan.nextLine();
 
         System.out.println("My name is "+name);
         System.out.println("My age is "+age);
-        System.out.println("My address is"+address);
+        System.out.println("My address is "+address);
 
     }
     
