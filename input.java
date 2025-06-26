8e3711cff74f0cac59fd42a5ec8b4560d9784958
Ilakkiyaa <ilakkiyaajaisankar@gmail.com>
2025-06-26 13:11:32 +0530

    Add input.java

--- input.java
+++ input.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 class hello{
     public static void main(String args[])
     {
-        Scanner var= new Scanner(System.in);
-        int a=var.nextInt();
-        int b=var.nextInt();
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         System.out.print(a+b);
+        sc.close();
+
     }
 }
